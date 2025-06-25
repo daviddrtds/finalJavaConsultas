75cc5c700efd6bd108a54cf4ed2a86d9486861b7
Joana Perpétuo <joana.perpetuo.263162630@msft.cesae.pt>
2025-06-25 15:14:46 +0100

    teste

---
+++ src/main/java/com/SGC/projeto_final_java/ProjetoFinalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.SGC.projeto_final_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalJavaApplication.class, args);
+	}
+
+}
